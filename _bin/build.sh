00510a01140359c809093cdb0571ab93827c70e5
anna-wilson <30553780+anna-wilson@users.noreply.github.com>
2024-01-08 23:10:11 +0000

    assets & layouts

--- _bin/build.sh
+++ _bin/build.sh
@@ -2,4 +2,4 @@
 curl -o _data/remote/groups.json https://dis.drachenwald.sca.org/data/regnum-groups.json
 bundle update --bundler
 bundle install
-bundle exec jekyll serve --livereload --watch --host 0.0.0.0
+bundle exec jekyll serve --livereload --watch --host 0.0.0.0 --port 4001
